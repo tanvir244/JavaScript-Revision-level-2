e234154b12b5449356e0d768cdcbfa5d2d427145
tanvir243 <travelertanvir@gmail.com>
2024-02-26 01:32:46 +0600

    problem-3 solved

---
+++ Solution/3.js
@@ -0,0 +1,19 @@
+// problem - 3
+const numbers = [2, 4, 6, 8];
+
+const makeAverage = () => {
+    const squaredNumbers = [];
+    const sumNumbers = [];
+    let sum = 0;
+    for(const number of numbers){
+        const squared = Math.pow(number, 2);
+        squaredNumbers.push(squared);
+    }
+    for(const squaredNumber of squaredNumbers){
+        sum = sum + squaredNumber;
+        sumNumbers.push(sum);
+    }
+    const finalAverage = sum / sumNumbers.length;
+    console.log(finalAverage);
+}
+makeAverage();
